6a192807da42abf79768dfabf8343e448c5c4294
James Wright <jwizzle.uk@gmail.com>
2015-12-03 17:39:03 +0000

    Define L

--- misc/tutorial.rkt
+++ misc/tutorial.rkt
@@ -1,2 +1,14 @@
 #lang racket
 (require redex)
+
+(define-language L
+  (e (e e)
+     (λ (x t) e)
+     x
+     (amb e ...)
+     number
+     (+ e ...)
+     (if0 e e e)
+     (fix e))
+  (t (→ t t) num)
+  (x variable-not-otherwise-mentioned))
