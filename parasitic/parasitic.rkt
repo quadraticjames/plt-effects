be6109de33a90e574cd903f337cae287b0423cdd
James Wright <jwizzle.uk@gmail.com>
2016-01-22 16:08:45 +0000

    Define language

--- parasitic/parasitic.rkt
+++ parasitic/parasitic.rkt
@@ -1,2 +1,20 @@
 #lang racket
 (require redex)
+
+(define-language Par
+  (e x
+     v
+     (e e)
+     (spawn e)
+     (parasite e)
+     chan
+     (send e e)
+     (recv e))
+  (v unit
+     (λ x e r)
+     c
+     κ)
+  (x variable-not-otherwise-mentioned)
+  (r (ρ e))
+  (c (ch x))
+  (κ (con v))) 
