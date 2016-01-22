7f7da560b027798ea7d25d7009a9ddd0aac1dae7
James Wright <jwizzle.uk@gmail.com>
2016-01-22 16:30:49 +0000

    Complete definition

--- parasitic/parasitic.rkt
+++ parasitic/parasitic.rkt
@@ -1,23 +1,44 @@
 #lang racket
 (require redex)
 
-(define-language Par
+(define-language Pt
   (e x
      v
      (e e)
-     (spawn e)
-     (parasite e)
+     (spawnHost e)
+     (spawnParasite e)
+     (attach e)
+     (prepare e e)
+     (reify e)
+     (inflate e)
      chan
      (send e e)
      (recv e))
   (v unit
      (λ x e r)
      c
-     κ)
-  (x variable-not-otherwise-mentioned)
-  (r (ρ e))
-  (c (ch x))
+     κ
+     k)
   (κ (const v))
+  (c (ch x))
+  (x variable-not-otherwise-mentioned)
+  (t (th x))
+  (r (ρ x e))
+  (k (f ...))
   (n (e r k_0 k_1 ...))
-  (k cont))
+  (f (ret v)
+     (arg e x)
+     (fun v)
+     recv
+     (sval e r)
+     (send v)
+     (rblk v)
+     (sblk v_1 v_2)
+     (pval e x)
+     (prep v))
+  (T (τ x s))
+  (C (χ x (k_0 ... & k_1 ...)))
+  (s (control e r k_0 k_1 ...)
+     (return k ...)
+     (halt v)))
 
