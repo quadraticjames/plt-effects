c4f6a9e2ea2e04065cb386ddf38bbb3a973094bb
James Wright <jwizzle.uk@gmail.com>
2016-03-11 15:02:47 +0000

    Add parasite stack

--- parasitic/parasitic.rkt
+++ parasitic/parasitic.rkt
@@ -16,8 +16,8 @@
   (f (ret v)
      (arg e r)
      (fun v))
-  (s (control e r k_0)
-     (return k)
+  (s (control e r k_0 k_1 ...)
+     (return k_0 k_1 ...)
      (halt v)))
 
 (define-extended-language Ev Pt
@@ -30,53 +30,53 @@
   (reduction-relation
    Ev
    #:domain p
-   (--> (control v r (f ...))
-        (return ((ret v) f ...))
+   (--> (control v r (f ...) k ...)
+        (return ((ret v) f ...) k ...)
         "value")
    #|
-(control unit (ρ) ())
+(control unit (ρ) () ())
 should reduce to
-(return ((ret unit)))
+(return ((ret unit)) ())
    |#
-   (--> (control x r (f ...))
-        (return ((ret (var-lookup r x)) f ...))
+   (--> (control x r (f ...) k ...)
+        (return ((ret (var-lookup r x)) f ...) k ...)
         "variable")
    #|
-(control x (ρ (x unit)) ())
+(control x (ρ (x unit)) () ())
 should reduce to
-(return ((ret unit)))
+(return ((ret unit)) ())
    |#
-   (--> (control (λ x e) r (f ...))
-        (return ((ret (λ x e r)) f ...))
+   (--> (control (λ x e) r (f ...) k ...)
+        (return ((ret (λ x e r)) f ...) k ...)
         "closure")
    #|
-(control (λ x x) (ρ) ())
+(control (λ x x) (ρ) () ())
 should reduce to
-(return ((ret (λ x x (ρ)))))
+(return ((ret (λ x x (ρ)))) ())
    |#
-   (--> (control (e_1 e_2) r (f ...))
-        (control e_1 r ((arg e_2 r) f ...))
+   (--> (control (e_1 e_2) r (f ...) k ...)
+        (control e_1 r ((arg e_2 r) f ...) k ...)
         "application")
    #|
-(control ((λ x x) unit) (ρ) ())
+(control ((λ x x) unit) (ρ) () ())
 should reduce to
-(control (λ x x) (ρ) ((arg unit (ρ))))
+(control (λ x x) (ρ) ((arg unit (ρ))) ())
    |#
-   (--> (return ((ret v) (arg e r) f ...))
-        (control e r ((fun v) f ...))
+   (--> (return ((ret v) (arg e r) f ...) k ...)
+        (control e r ((fun v) f ...) k ...)
         "argument")
    #|
-(return ((ret (λ x x (ρ))) (arg unit (ρ))))
+(return ((ret (λ x x (ρ))) (arg unit (ρ))) ())
 should reduce to
-(control unit (ρ) ((fun (λ x x (ρ)))))
+(control unit (ρ) ((fun (λ x x (ρ)))) ())
    |#
-   (--> (return ((ret v) (fun (λ x e (ρ (x_0 v_0) ...))) f ...))
-        (control e (ρ (x_0 v_0) ... (x v)) (f ...))
+   (--> (return ((ret v) (fun (λ x e (ρ (x_0 v_0) ...))) f ...) k ...)
+        (control e (ρ (x_0 v_0) ... (x v)) (f ...) k ...)
         "function")
    #|
-(return ((ret unit) (fun (λ x x (ρ)))))
+(return ((ret unit) (fun (λ x x (ρ)))) ())
 should reduce to
-(control x (ρ (x unit)) ())
+(control x (ρ (x unit)) () ())
    |#
    ))
 
